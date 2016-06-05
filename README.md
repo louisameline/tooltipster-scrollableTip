836a186d2d40111b961285a8979a9a627307fc42
louisameline <evangun2001@yahoo.fr>
2016-06-06 01:56:36 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # tooltipster-scrollableTip
-A Tooltipster plugin to have scrolling within the tooltip rather than viewport overflow
+A Tooltipster plugin to make tooltips scrollable when they get too big.
+
+When the tooltip is about to overflow the viewport, this plugin does the following: put the tooltip on the side which maximizes its size, resize it so it doesn't overflow the viewport, and make it scrollable for the content to be accessible.
+
+The tooltip will be off screen only if the origin itself is off screen.
+
+Installation
+------------
+
+Include the plugin file in your page AFTER the Tooltipster bundle file.
+
+```html
+<html>
+    <head>
+        ...
+        <script type="text/javascript" src="tooltipster/dist/js/tooltipster.bundle.min.js"></script>
+        <script type="text/javascript" src="tooltipster-scrollableTip/tooltipster-scrollableTip.min.js"></script>
+    </head>
+</html>
+```
+
+> This plugin only works in conjunction with Tooltipster's default `sideTip` plugin.
+
+Usage
+-----
+
+Declare the `scrollableTip` plugin in the options of the tooltips you want to be scrollable, alongside `sideTip`:
+
+```javascript
+$('.tooltip').tooltipster({
+    plugins: ['tooltipster.sideTip', 'laa.scrollableTip']
+});
+```
+
+> Pro tip: for a better style, know that the scrollbar is customizable with CSS in Chrome
+
+Simple as that!
---
+++ tooltipster-scrollableTip.js
@@ -0,0 +1,206 @@
+/**
+ * tooltipster-scrollableTip v1.0.0
+ * https://github.com/louisameline/tooltipster-scrollableTip/
+ * Developed by Louis Ameline
+ * MIT license
+ */
+(function(root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module unless amdModuleId is set
+		define(['tooltipster'], function ($) {
+			return (factory($));
+		});
+	}
+	else if (typeof exports === 'object') {
+		// Node. Does not work with strict CommonJS, but
+		// only CommonJS-like environments that support module.exports,
+		// like Node.
+		module.exports = factory(require('tooltipster'));
+	}
+	else {
+		factory(jQuery);
+	}
+}(this, function($) {
+	
+	$.tooltipster.plugin({
+		name: 'laa.scrollableTip',
+		instance: {
+			_defaults: function() {
+				
+				return {
+					// in all cases, the tooltip will be limited to the size
+					// of the viewport. But if this option is set to true, we
+					// will limit its size even further to make the tooltip fit
+					// on screen.
+					viewportFit: false
+				};
+			},
+			_init: function(instance) {
+				
+				var self = this;
+				
+				// list of instance variables
+				
+				self.instance = instance;
+				self.maxSize;
+				self.namespace = 'tooltipster-scrollableTip-'+ Math.round(Math.random()*100000);
+				self.options;
+				
+				// initial formatting
+				self._optionsFormat();
+				
+				// reformat every time the options are changed
+				self.instance._on('options', function() {
+					self._optionsFormat();
+				});
+				
+				// prevent the tests on the document to save time
+				self.instance._on('positionTest', function(event) {
+					if (event.container == 'document') {
+						event.takeTest(false);
+					}
+				});
+				
+				// select the scenario that will give a maximum area to the tooltip
+				self.instance._on('positionTested', function(event) {
+					
+					var whole = false;
+					
+					$.each(event.results, function(i, result) {
+						
+						// if the tooltip completely fits on screen on one of the
+						// sides, there is nothing to do
+						if (result.whole) {
+							whole = true;
+							return false;
+						}
+					});
+					
+					if (!whole) {
+						
+						var maxSizes = [],
+							biggestArea,
+							index;
+						
+						// find out on which side the tooltip would have the biggest area after we
+						// restrain its size to the size of the viewport
+						$.each(event.results, function(i, result) {
+							
+							// the available height may be greater than the viewport height if the
+							// origin is off screen at the top or bottom
+							maxSizes[i] = {
+								height: Math.min(
+									event.helper.geo.available.window[result.side].height - result.distance.vertical,
+									event.helper.geo.window.size.height
+								),
+								width: Math.min(
+									event.helper.geo.available.window[result.side].width - result.distance.horizontal,
+									event.helper.geo.window.size.width
+								)
+							};
+							
+							// this dismisses natural size scenarios where the tooltip is too big
+							// (the constrained scenario for that side will used instead), but not the
+							// natural size scenarios where it's narrower than the available space (in
+							// that cases there is no constrained test for that side because it wasn't
+							// needed)
+							if (result.size.width <= maxSizes[i].width) {
+								
+								var height = Math.min(result.size.height, maxSizes[i].height),
+									width = Math.min(result.size.width, maxSizes[i].width),
+									area = height * width;
+								
+								// if 2 areas are equal, the first one is preferred (came first because
+								// it had a higher priority at the time of measuring)
+								if (!biggestArea || area > biggestArea) {
+									biggestArea = area;
+									index = i;
+								}
+							}
+						});
+						
+						// leave only the wanted scenario
+						event.edit([event.results[index]]);
+						
+						// save for the position event listener
+						self.maxSize = maxSizes[index];
+					}
+					else {
+						self.maxSize = null;
+					}
+				});
+				
+				// restrain the size
+				self.instance._on('position', function(event) {
+					
+					var pos = event.position;
+					
+					// in case there already was a listener.
+					// Note: we don't need to unbind at closing time, sideTip already
+					// clears the tooltip
+					$(event.tooltip).off('.'+ self.namespace);
+					
+					if (self.maxSize) {
+						
+						if (	pos.size.height > self.maxSize.height
+							&&	pos.side !== 'bottom'
+						) {
+							pos.coord.top = 0;
+						}
+						if (	pos.size.width > self.maxSize.width
+							&&	pos.side !== 'right'
+						) {
+							pos.coord.left = 0;
+						}
+						
+						pos.size.height = Math.min(pos.size.height, self.maxSize.height);
+						pos.size.width = Math.min(pos.size.width, self.maxSize.width);
+						
+						event.edit(pos);
+						
+						if (!self.instance.options.interactive) {
+							
+							// we have to make the tooltip interactive ourselves. Touch events will
+							// emulate mouse events, we don't really care for the difference at this
+							// point (unless somebody comes up with a good use case)
+							$(event.tooltip)
+								.css('pointer-events', 'auto')
+								.on('mouseenter.'+ self.namespace, function(event) {
+									self.instance._trigger({
+										dismissable: false,
+										type: 'dismissable'
+									});
+								})
+								.on('mouseleave.'+ self.namespace, function(event) {
+									self.instance._trigger({
+										// we don't bother to differentiate mouse and touch, so we'll just
+										// use the touch delay which is longer by default
+										delay: self.instance.options.delayTouch[1],
+										dismissable: true,
+										event: event,
+										type: 'dismissable'
+									});
+								});
+						}
+					}
+					else {
+						
+						// in case we had previously made it interactive
+						if (!self.instance.options.interactive) {
+							$(event.tooltip).css('pointer-events', '');
+						}
+					}
+				});
+			},
+			_optionsFormat: function() {
+				
+				var defaults = this._defaults(),
+				// if the plugin options were isolated in a property named after the
+				// plugin, use them (prevents conflicts with other plugins)
+					pluginOptions = this.instance.options[this.name] || this.instance.options;
+				
+				this.options = $.extend(true, {}, defaults, pluginOptions);
+			}
+		}
+	});
+}));
